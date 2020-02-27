40721cf35b1324fb84c3a53e9ddad9bef8b7b49b
Joseph_Tully <tully4school@gmail.com>
2020-02-26 21:11:19 -0500

    added stylesheet and created basic splash for staff

--- public/staff/index.php
+++ public/staff/index.php
@@ -2,11 +2,23 @@
 
 <html lang="en">
   <head>
-    <title></title>
+    <title>GBI</title>
     <meta charset="utf-8">
   </head>
 
   <body>
-
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
+    <navigation>
+    <ul>
+      <li>
+      <a href="index.php">Menu</a>
+      </li>
+    </ul>
+    </navigation>
+    <footer>
+    &copy; <?php echo date('Y');?> Globe Bank
+    </footer>
   </body>
 </html>
