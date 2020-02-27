24c9b59136f9afd450a6067cef14d5b2dffe3969
Joseph_Tully <tully4school@gmail.com>
2020-02-27 01:36:59 -0500

    added a function to ecncode url

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,3 +1,5 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php 
     // -------AWESOME NEW TERNARY-------
     $id = $_GET['id'] ?? '1'; 
@@ -5,3 +7,7 @@
     echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('Jon Doe'); ?>">Link</a>
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a>
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a>
