50d3a0b79f97f06de3fbdcc155c2131cd61d4ee0
Joseph_Tully <tully4school@gmail.com>
2020-02-27 20:59:03 -0500

    added to functions to see whether or not a form has been submitted

--- private/functions.php
+++ private/functions.php
@@ -31,5 +31,15 @@ function redirect_to($location) {
     header("Location: " . $location);
     exit;
 }
+
+//to see whether or not a form has been submitted
+
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST'
+}
+
+function is_get_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
  
 ?>
